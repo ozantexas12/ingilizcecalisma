ac04127d0961d6d5c5f9b21980854203b82f385f
ozanpaker <ozanpaker72@gmail.com>
2024-03-27 16:07:30 +0300

    Temprory Page

---
+++ lib/pages/temprory.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TemproryPage extends StatefulWidget {
+  const TemproryPage({super.key});
+
+  @override
+  State<TemproryPage> createState() => _TemproryPageState();
+}
+
+class _TemproryPageState extends State<TemproryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      g
+    );
+  }
+}
