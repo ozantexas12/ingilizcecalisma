37c19085465fe00159438c5c234db056b2770430
ozanpaker <ozanpaker72@gmail.com>
2024-04-16 15:11:20 +0300

    Ana Sayfada sol üstte ki drawerın içine uygulamanın sürümü logosu eklendi.

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ingilizcecalisma/hızlı/color.dart';
 import 'package:ingilizcecalisma/pages/listpage.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,15 +17,92 @@ enum Lang { tr, eng }
 class _HomePageState extends State<HomePage> {
   Lang? _chooseLang = Lang.eng;
 
-  final GlobalKey<ScaffoldState> _scaffoldKey= GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  PackageInfo ?packageInfo;
+  String version="";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    packageInfoInit();
+  }
+
+
+  void packageInfoInit() async {
+    packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      version=packageInfo!.version;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      drawer: Container(
-        width: MediaQuery.of(context).size.width*0.5,
-        color: Colors.white,
+      drawer: SafeArea(
+        child: Container(
+          width: MediaQuery
+              .of(context)
+              .size
+              .width * 0.5,
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Image.asset("assets/images/logo.png", height: 80,),
+                  const Text(
+                    "Easy",
+                    style: TextStyle(fontSize: 25, fontFamily: "RobotoRegular"),
+                  ),
+                  const Text(
+                    "İstediğini öğren",
+                    style: TextStyle(fontSize: 16, fontFamily: "RobotoRegular"),
+                  ),
+                  SizedBox(
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.30,
+                    child: Divider(),
+                  ),
+                  Container(margin: EdgeInsets.only(top: 50, left: 8, right: 8),
+                    child: const Text(
+                      "Burası dolucak.",
+                      style: TextStyle(
+                          fontSize: 16, fontFamily: "RobotoRegular"),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "Tıkla",
+                      style: TextStyle(fontSize: 16,
+                          fontFamily: "RobotoRegular",
+                          color: Color(Ok.HexaColorConverter("#0A588D"))),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "v"+version+"\ndeneme@gmail.com",
+                  style: TextStyle(fontSize: 16,
+                      fontFamily: "RobotoRegular",
+                      color: Color(Ok.HexaColorConverter("#0A588D"))),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+            ],
+          ),
+        ),
       ),
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(50),
@@ -35,11 +114,14 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.05,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height * 0.05,
                 child: InkWell(
-                  onTap:(){
+                  onTap: () {
                     _scaffoldKey.currentState!.openDrawer();
-                  } ,
+                  },
                   child: const FaIcon(
                     FontAwesomeIcons.bars,
                     color: Colors.black,
@@ -48,11 +130,17 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.10,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height * 0.10,
                 child: Image.asset("assets/images/logo_text.png"),
               ),
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.15,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height * 0.15,
               ),
             ],
           ),
@@ -75,14 +163,22 @@ class _HomePageState extends State<HomePage> {
                 const SizedBox(
                   height: 20,
                 ),
-                InkWell(onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const ListsPage(),));
-                },
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ListsPage(),
+                        ));
+                  },
                   child: Container(
                     margin: const EdgeInsets.only(bottom: 20),
                     alignment: Alignment.center,
                     height: 55,
-                    width: MediaQuery.of(context).size.width * 0.8,
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.8,
                     decoration: BoxDecoration(
                       color: Colors.red,
                       borderRadius: const BorderRadius.all(
@@ -108,7 +204,10 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.8,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width * 0.8,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
