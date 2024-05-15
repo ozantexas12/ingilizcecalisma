d20495ca526964b6e67338fb42e9b50782ccc8c7
ozanpaker <ozanpaker72@gmail.com>
2024-05-15 17:58:27 +0300

    Listeye eleman ekle, Listeye kelime ekle, Liste Id'sine göre kelimeleri getir ve bütün kelimeleri getire ait methodlar yazıldı.

--- lib/database/db/database.dart
+++ lib/database/db/database.dart
@@ -38,13 +38,6 @@ class DatabaseProvider {
 
 
 
-
-
-
-
-
-
-
     await db.execute('''
       CREATE TABLE IF NOT EXİSTS $tableNameWords(
         ${WordTableFields.id} INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -56,4 +49,35 @@ class DatabaseProvider {
       )
     ''');
   }
+
+  Future<Lists> insertList(Lists lists) async{
+    final db =await instance.database;
+    final id = await db.insert(tableNameLists, lists.toJson());
+
+    return lists.copy(id: id);
+  }
+
+  Future<Word> insertWord(Word word) async {
+    final db = await instance.database;
+    final id = await db.insert(tableNameWords, word.toJson());
+    return word.copy(id: id);
+  }
+
+  Future<List<Word>> readWordByList(int ?listsID) async{
+    final db=await instance.database;
+    const orderBy='${WordTableFields.id} ASC';
+    final result = await db.query(tableNameWords,orderBy: orderBy,where: '${WordTableFields.list_id}= ?',whereArgs: [listsID]);
+    
+    return result.map((json) => Word.fromJson(json)).toList();
+  }
+  Future<List<Lists>> readListsAll() async{
+    final db=await instance.database;
+    const orderBy='${ListsTableFields.id} ASC';
+    final result = await db.query(tableNameLists,orderBy: orderBy);
+    return result.map((json) => Lists.fromJson(json)).toList();
+  }
+
+
+
+
 }
